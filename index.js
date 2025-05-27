e53ce3f585d3987141475b53962ead7be68b7c87
unknown <samuelnascimento6282@digitalcollege.internal>
2025-05-27 19:33:35 -0300

    ainda na primeira

---
+++ index.js
@@ -0,0 +1,11 @@
+function calcular() {
+    const horas = Number(document.getElementById('horas'));
+    const valorHora = Number(document.getElementById('valorHoras'))
+    let resultado = '';
+}
+
+function calcularPagamento() {
+    const total = horas * valorHora;
+    resultado = `Total a receber: R$${total}`;
+    document.getElementById('resultado').innerHTML = resultado
+}
